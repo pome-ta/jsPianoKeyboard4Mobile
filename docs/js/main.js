70b9c9bab0c967000b05ae4a9f515905fd628331
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-13 13:55:37 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('hoge');
+const octaveKeyPatterns = [
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+];
+
+const pianoKeysWrap = document.createElement('div');
+pianoKeysWrap.style.widtfalse = '100%';
+
+for (const keyPattern of octaveKeyPatterns) {
+  const key = document.createElement('span');
+  key.classList.add('pianokey');
+  keyPattern ? key.classList.add('sharp') : null;
+  pianoKeysWrap.appendChild(key);
+}
+
+document.body.appendChild(pianoKeysWrap);
