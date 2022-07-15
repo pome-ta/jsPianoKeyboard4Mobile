e935c7c78401040dd32aea928e185cd4de6d1b17
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-15 15:28:54 +0900

    qwerty-hancock set

--- docs/js/main.js
+++ docs/js/main.js
@@ -37,10 +37,90 @@ const octaveKeyPatterns = [
 ];
 
 const pianoKeysWrap = document.createElement('div');
-pianoKeysWrap.style.widtfalse = '100%';
+pianoKeysWrap.style.width = '100%';
 
 for (const keyPattern of octaveKeyPatterns) {
   setupKey(keyPattern, pianoKeysWrap);
 }
 
 document.body.appendChild(pianoKeysWrap);
+
+const synth = document.createElement('div');
+const keyboardDiv = document.createElement('div');
+keyboardDiv.id = 'keyboard';
+
+
+synth.classList.add('synth');
+synth.appendChild(keyboardDiv);
+document.body.appendChild(synth);
+
+
+
+const keyboardWidth = document.querySelector('.synth').clientWidth;
+
+const settings = {
+  id: 'keyboard',
+  width: `${keyboardWidth}`,
+  height: 128,
+  startNote: 'A2',
+  margin: 'auto',
+  whiteNotesColour: '#fff',
+  blackNotesColour: '#000',
+  borderColour: '#000',
+  activeColour: 'maroon',
+  octaves: 2,
+  musicalTyping: false
+}
+
+let keyboard = new window.QwertyHancock(settings);
+
+
+
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const context = new AudioContext();
+
+const masterGain = context.createGain();
+
+
+masterGain.gain.value = 0.5;
+
+masterGain.connect(context.destination);
+
+
+// xxx: アナライザが先かマスターが先か
+//analyzeNode.connect(masterGain);
+//masterGain.connect(context.destination);
+
+let nodes = [];
+keyboard.keyDown = (note, frequency) => {
+  const oscillator = context.createOscillator();
+  //oscillator.type = 'sine';
+  oscillator.type = 'square';
+  //oscillator.type = 'sawtooth';
+  //oscillator.type = 'triangle';
+  oscillator.frequency.value = frequency;
+  
+  //oscillator.connect(analyzeNode);
+  oscillator.connect(masterGain);
+  oscillator.start(0);
+  
+  nodes.push(oscillator);
+};
+
+
+
+keyboard.keyUp = (note, frequency) => {
+  const newNodes = [];
+
+  for (const nd of nodes) {
+    if (Math.round(nd.frequency.value) === Math.round(frequency)) {
+      nd.stop(0);
+      nd.disconnect();
+    } else {
+      newNodes.push(nd);
+    }
+  }
+
+  nodes = newNodes;
+};
