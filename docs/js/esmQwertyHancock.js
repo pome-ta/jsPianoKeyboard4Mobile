9038ec6f1f2d84db7a131c20e3ac8b158f484120
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-15 16:11:02 +0900

    up

---
+++ docs/js/esmQwertyHancock.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let QwertyHancock;
+const version = '0.0.0';
+
+const settings = {
+  id: 'keyboard',
+  octaves: 3,
+  width: 0,
+  height: 0,
+  margin: 0,
+  startNote: 'A3',
+  whiteKeyColour: '#fff',
+  blackKeyColour: '#000',
+  activeColour: 'yellow',
+  borderColour: '#000',
+  keyboardLayout: 'en',
+  musicalTyping: false,
+};
+
+const init = function (us) {
+  const user_settings = us || {};
+};
